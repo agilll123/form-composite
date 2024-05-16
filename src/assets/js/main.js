8888adcd9c92f7ad0e68e08b099f324d8f49f051
agilll123 <dmitrjijkiricenko26@gmail.com>
2024-05-16 19:47:44 +0300

    header-burger

---
+++ src/assets/js/main.js
@@ -0,0 +1,10 @@
+"use strict"
+window.onload = function(
+{
+	mainObj.init()
+}
+{
+	let mainObj = 
+	init()
+}
+)
---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+mainObj.header =
+{
+	show()
+	{
+		if(instance.classList.contains('active'))
+		{
+			instance.classLIst.remove('active')
+		}
+		elce
+		{
+			imstance.classLIst.add('active')
+		}
+	}
+}
